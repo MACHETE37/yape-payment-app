b87e40452489ffd42f4985d3c8d5550c4f274725
MACHETE37 <vangelcusi@aol.com>
2025-05-06 14:21:26 -0500

    listo backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SearchUserComponent } from './YapeApp/search-user/search-user.component';
+import { PaymentMethodComponent } from './YapeApp/payment-method/payment-method.component';
+import { PaymentConfirmationComponent } from './YapeApp/payment-confirmation/payment-confirmation.component';
+import { PaymentMethodSelectorComponent } from './YapeApp/payment-method-selector/payment-method-selector.component';
+import { CreditCardPaymentComponent } from './YapeApp/credit-card-payment/credit-card-payment.component';
+import { DniRucRegisterComponent } from './dni-ruc-register/dni-ruc-register.component';
+export const routes: Route[] = [
+    { path: '', component: LoginComponent },
+    { path: 'search-user', component: SearchUserComponent },
+    { path: 'payment', component: PaymentMethodComponent },
+    { path: 'confirmation', component: PaymentConfirmationComponent },
+    { path: 'select-method', component: PaymentMethodSelectorComponent },
+    { path: 'credit-card', component: CreditCardPaymentComponent },
+    { path: 'yape-plin', component: PaymentMethodComponent }, // Usar componente existente modificado
+    { path: 'registrar',component:DniRucRegisterComponent},
+    { path: '**', redirectTo: '' }
+];
 
-export const routes: Routes = [];
+//{path: '', loadComponent: () => import('app/modules/admin/ControlInterno/apertura-procesos/apertura-procesos.component')},
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { PaymentMethodComponent } from './app/YapeApp/payment-method/payment-method.component';
-import { PaymentConfirmationComponent } from './app/YapeApp/payment-confirmation/payment-confirmation.component';
-import { SearchUserComponent } from './app/YapeApp/search-user/search-user.component';
-import { LoginComponent } from './app/login/login.component';
-import { PaymentMethodSelectorComponent } from './app/YapeApp/payment-method-selector/payment-method-selector.component';
-import { CreditCardPaymentComponent } from './app/YapeApp/credit-card-payment/credit-card-payment.component';
-import { DniRucRegisterComponent } from './app/dni-ruc-register/dni-ruc-register.component';
+import { appConfig } from './app/app.config'; // Importa la configuración
 
-  bootstrapApplication(AppComponent, {
-    providers: [
-      provideRouter([
-        { path: '', component: LoginComponent },
-        { path: 'search-user', component: SearchUserComponent },
-        { path: 'payment', component: PaymentMethodComponent },
-        { path: 'confirmation', component: PaymentConfirmationComponent },
-        { path: 'select-method', component: PaymentMethodSelectorComponent },
-        { path: 'credit-card', component: CreditCardPaymentComponent },
-        { path: 'yape-plin', component: PaymentMethodComponent }, // Usar componente existente modificado
-        { path: 'registrar',component:DniRucRegisterComponent},
-        { path: '**', redirectTo: '' }
-      ])
-    ]
-  });
+  bootstrapApplication(AppComponent,appConfig, );
